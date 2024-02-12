bc84928e4f32639119811f71f5ec3a1a6e78f7a8
Sobir Bobojonov <121563197+BobojonovSobir0576@users.noreply.github.com>
2024-02-12 09:31:42 +0500

    Update category.py
    add category for admin

--- back-end/config/views/category.py
+++ back-end/config/views/category.py
@@ -13,7 +13,7 @@ from main_services.responses import (
     user_not_found_response,
     unauthorized_response
 )
-from main_services.roles import custom_user_has_client_role, custom_user_has_patient_role
+from main_services.roles import custom_user_has_client_role, custom_user_has_patient_role, custom_user_has_admin_role
 from main_services.swaggers import swagger_extend_schema, swagger_schema
 from main_services.expected_fields import check_required_key
 from authentification.models import Categories
@@ -38,6 +38,10 @@ class CategoryViews(APIView):
             category = Categories.objects.select_related('groups').filter(
                 Q(groups__name='doctor')
             )
+            serializer = CategoriesSerializer(category, many=True, context={'request': request})
+            return success_response(serializer.data)
+        if custom_user_has_admin_role(request.user):
+            category = Categories.objects.all()
             serializer = CategoriesSerializer(category, many=True, context={'request': request})
             return success_response(serializer.data)
 
